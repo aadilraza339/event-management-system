98348940426807d18d502a8451609b93a9be4f31
harshitacodes <harshitagupta608@gmail.com>
2020-08-16 02:31:18 +0530

    create, update, delete has done

--- app/routes/event.js
+++ app/routes/event.js
@@ -2,7 +2,10 @@ const Event = require('../service/Event/Event')
 const express = require('express')
 const event = express.Router()
 
-event.post('/CreateEvent', Event.CreateEvent)
+event.post('/createEvent', Event.CreateEvent)
+event.post('/editEvent/:eventId', Event.EditEvent)
+event.delete('/deleteEvent/:eventId', Event.DeleteEvent)
+event.get('/allEvents', Event.Events)
 
 // user.post('/login',User.login)
 
